e97164042c73e87171c9a1bd86d2f90100f40a2c
Joseph Bayly <jtbayly@gmail.com>
2021-06-23 14:06:02 -0400

    cleanup build scripts

--- files/scripts/_buildGHA.sh
+++ files/scripts/_buildGHA.sh
@@ -5,14 +5,14 @@ set -ev
 # Install fonts
 
 # encrypted via: "gpg -c font1.zip"
-echo NotASecret | gpg --batch --yes --passphrase-fd 0 -d --output files/fonts/font1.zip files/fonts/font1.zip.gpg
-unzip classics-template-files/fonts/font1.zip
-unzip classics-template-files/fonts/font2.zip
-sudo cp -vf Calluna/Calluna-Regular.otf /Library/Fonts
-sudo cp -vf LiberationSerif-Regular.ttf /Library/Fonts
-sudo cp -vf LiberationSerif-BoldItalic.ttf /Library/Fonts
-sudo cp -vf LiberationSerif-Bold.ttf /Library/Fonts
-sudo cp -vf LiberationSerif-Italic.ttf /Library/Fonts
+#echo NotASecret | gpg --batch --yes --passphrase-fd 0 -d --output files/fonts/font1.zip files/fonts/font1.zip.gpg
+#unzip classics-template-files/fonts/font1.zip
+#unzip classics-template-files/fonts/font2.zip
+#sudo cp -vf Calluna/Calluna-Regular.otf /Library/Fonts
+#sudo cp -vf LiberationSerif-Regular.ttf /Library/Fonts
+#sudo cp -vf LiberationSerif-BoldItalic.ttf /Library/Fonts
+#sudo cp -vf LiberationSerif-Bold.ttf /Library/Fonts
+#sudo cp -vf LiberationSerif-Italic.ttf /Library/Fonts
 
 # render web version
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
